320ad18afbc1325bd3fe338ecef9b245f3a31de9
Tomilov Alexander <sopholos@sopholos.ru>
2024-09-14 01:12:49 +0500

    test archive + bash pipe

--- files/postgre_backup_cluster.ps1
+++ files/postgre_backup_cluster.ps1
@@ -4,6 +4,7 @@ param(
 	[parameter(Mandatory=$true)][string]$PostgresqlUser,
 	[parameter(Mandatory=$true)][string]$PostgresqlHost,
 	[parameter(Mandatory=$true)][int]$PostgresqlPort,
+	[parameter(Mandatory=$false)][bool]$TestArchive = $false,
 	[parameter(Mandatory=$true)][string]$BackupFolder
 )
 $ErrorActionPreference = "Stop"
@@ -14,22 +15,35 @@ try
 	$date = Get-Date -Format "yyyy-MM-dd_HH-mm_ss.fff"
 	$BackupPath = Join-Path $BackupFolder -ChildPath "$date.7zbk"
 
-	pg_basebackup `
-		--progress `
-		--username=$PostgresqlUser `
-		--pgdata=- `
-		--wal-method=fetch `
-		--format=tar `
-		--checkpoint=fast `
-		--compress=0 `
-		--host=$PostgresqlHost `
-		--port=$PostgresqlPort `
-	| `
-	&$7zipPath `
-		a $BackupPath `
-		-si -bt -mx=1
+	$bashCommand = @"
+	pg_basebackup \
+		--username=$PostgresqlUser \
+		--pgdata=- \
+		--wal-method=fetch \
+		--format=tar \
+		--checkpoint=fast \
+		--compress=0 \
+		--host=$PostgresqlHost \
+		--port=$PostgresqlPort \
+	| \
+	$7zipPath \
+		a $BackupPath \
+		-si -bt -mx=1;
+
+	backupStatus=`$((`${PIPESTATUS[0]} | `${PIPESTATUS[1]}));
+	echo BackupStatus: `$backupStatus;
+	exit `$backupStatus;
+"@
+	bash -c $bashCommand
 
 	if ($LASTEXITCODE -ne 0) { throw "pg_basebackup exited with code $LASTEXITCODE." }
+
+	if ($TestArchive) {
+		&$7zipPath `
+			t $BackupPath
+
+		if ($LASTEXITCODE -ne 0) { throw "7z test exited with code $LASTEXITCODE." }
+	}
 
 	$doneFile = "$BackupPath.done"
 	$(Get-Date -format "yyyy-MM-dd HH:mm:ss") | Set-Content $doneFile
