f561145acbb4b71e16ee6854d4475bfac38e1b71
Tomilov Alexander <sopholos@sopholos.ru>
2024-09-14 12:48:08 +0500

    rewrite postgre_wal_read.ps1 to bash to speedup

---
+++ files/postgre_wal_read.sh
@@ -0,0 +1,68 @@
+#! /usr/bin/env bash
+# Ansible managed
+
+while [ $# -gt 0 ]; do
+    if [[ $1 == "-"* ]]; then
+        v="${1/-/}"
+        declare "$v"="$2"
+        shift
+    fi
+    shift
+done
+
+echo "\ArchiveFullPath: '$ArchiveFullPath' \ArchiveName: '$ArchiveName' \BackupPath: '$BackupPath'"
+
+backwal=$BackupPath
+ext7z=".7z"
+walFile=$backwal/$ArchiveName
+walFile7z=$walFile$ext7z
+
+echo "\walFile: '$walFile' \walFile7z: '$walFile7z'"
+
+if [ ! -f $walFile ]; then
+	walFile=$backwal/oldstorage/$ArchiveName
+fi
+
+if [ -f $walFile ]; then
+	echo Copying $walFile $ArchiveFullPath
+	cp $walFile $ArchiveFullPath
+	exit $?;
+fi
+
+if [ ! -f $walFile7z ]; then
+	walFile7z=$backwal/oldstorage/$ArchiveName$ext7z
+fi
+
+if [ -f $walFile7z ]; then
+	tmpdst="/tmp/wal_copy"
+	echo Copying $walFile7z $tmpdst
+	/bin/cp -rf -T $walFile7z $tmpdst
+	
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		echo failed to copy $walFile7z
+		exit $ret;	
+	fi
+
+	tmpfold="/tmp/wal_copy_ex"
+	mkdir -p $tmpfold
+
+	/usr/lib/p7zip/7z \
+		x "$tmpdst" \
+		-o"$tmpfold" \
+		-bd -aoa
+
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		echo failed to unarchive $walFile7z
+		exit $ret;	
+	fi
+
+	source=$tmpfold/$ArchiveName
+	/bin/mv -f $source $ArchiveFullPath
+
+    exit $?;
+fi
+
+echo file not found
+exit 1
