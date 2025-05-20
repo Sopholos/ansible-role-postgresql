4f4c866d98441c1bff0811007c0befbbe78701d5
Tomilov Alexander <sopholos@sopholos.ru>
2025-05-20 18:34:44 +0500

    dump to s3 support

--- files/postgre_dump_db.ps1
+++ files/postgre_dump_db.ps1
@@ -3,10 +3,14 @@
 param(
 	[parameter(Mandatory=$true)][string]$PostgresqlUser,
 	[parameter(Mandatory=$true)][string]$PostgresqlPassword,
+	[parameter(Mandatory=$false)][string]$QueryPostgresqlUser,
+	[parameter(Mandatory=$false)][string]$QueryPostgresqlPassword,
 	[parameter(Mandatory=$true)][string]$PostgresqlHost,
 	[parameter(Mandatory=$true)][int]$PostgresqlPort,
 	[parameter(Mandatory=$true)][string]$Database,
-	[parameter(Mandatory=$true)][string]$BackupFolder
+	[parameter(Mandatory=$true)][string]$BackupFolder,
+	[parameter(Mandatory=$false)][AllowEmptyString()][string]$s3Endpoint = '',
+	[parameter(Mandatory=$false)][AllowEmptyString()][string]$s3Profile = ''
 )
 $ErrorActionPreference = "Stop"
 
@@ -20,32 +24,117 @@ try
 	if ((/usr/local/bin/Get-PGIsInRecovery `
 			-PostgresqlHost $PostgresqlHost `
 			-PostgresqlPort $PostgresqlPort `
-			-PostgresqlUser $PostgresqlUser) -eq $true) {
+			-PostgresqlUser $QueryPostgresqlUser `
+			-PostgresqlPassword $QueryPostgresqlPassword) -eq $true) {
 		Write-Host -ForegroundColor Yellow "Postgresql is in restoring state"
 		return;
 	}
 
 	$date = Get-Date -Format "yyyy-MM-dd_HH-mm_ss.fff"
+	$dburl = "postgresql://${PostgresqlUser}:$PostgresqlPassword@${PostgresqlHost}:$PostgresqlPort/$Database"
 
-	New-Item -ItemType Directory -Force -Path $BackupFolder
+	if ($s3Endpoint -or $s3Profile -or $BackupFolder.StartsWith('s3://')) {
+		$backupFile = "$BackupFolder/${Database}_$date.dmbk"
 
-	$backupFile = Join-Path $BackupFolder "${Database}_$date.dmbk"
+		$pgArgs = @(
+			"--dbname=$dburl",
+			"--format=c",
+			"--compress=1"
+		)
 
-	pg_dump `
-		--dbname=postgresql://${PostgresqlUser}:$PostgresqlPassword@${PostgresqlHost}:$PostgresqlPort/$Database `
-		--format=c `
-		--compress=1 `
-		--file=$backupFile
+		$awsArgs = @(
+			"s3", "cp",
+			"-",
+			$backupFile
+		)
+		if ($s3Endpoint) {
+			$awsArgs +="--endpoint-url", $s3Endpoint
+		}
+		if ($s3Profile) {
+			$awsArgs += "--profile", $s3Profile
+		}
 
-	if ($LASTEXITCODE -ne 0) { throw "pg_dump exited with code $LASTEXITCODE." }
+		&pg_dump $pgArgs | &aws @awsArgs
 
-	$doneFile = $backupFile + ".done"
-	$(Get-Date -format "yyyy-MM-dd HH:mm:ss") | Set-Content $doneFile
+		if ($LASTEXITCODE -ne 0) { throw "aws s3 cp exited with code $LASTEXITCODE." }
+		Write-Host "Backed up $backupFile"
 
-	Write-Host -ForegroundColor Green "Backed up successfully"
+		$pgArgs = @(
+			"--list"
+		)
+
+		$awsArgs = @(
+			"s3", "cp",
+			$backupFile,
+			"-"
+		)
+		if ($s3Endpoint) {
+			$awsArgs +="--endpoint-url", $s3Endpoint
+		}
+		if ($s3Profile) {
+			$awsArgs += "--profile", $s3Profile
+		}
+
+		&aws @awsArgs |	&pg_restore $pgArgs
+
+		if ($LASTEXITCODE -ne 0) { throw "pg_restore exited with code $LASTEXITCODE." }
+		Write-Host "Validated $backupFile"
+
+		$doneFile = $backupFile + ".done"
+
+		$awsArgs = @(
+			"s3", "cp",
+			"-",
+			$doneFile
+		)
+		if ($s3Endpoint) {
+			$awsArgs +="--endpoint-url", $s3Endpoint
+		}
+		if ($s3Profile) {
+			$awsArgs += "--profile", $s3Profile
+		}
+
+		$(Get-Date -format "yyyy-MM-dd HH:mm:ss") | &aws @awsArgs
+
+		if ($LASTEXITCODE -ne 0) { throw "aws s3 cp done file exited with code $LASTEXITCODE." }
+		Write-Host "Wrote $doneFile"
+	}
+	else {
+		New-Item -ItemType Directory -Force -Path $BackupFolder
+
+		$backupFile = Join-Path $BackupFolder "${Database}_$date.dmbk"
+
+		$pgArgs = @(
+			"--dbname=$dburl",
+			"--format=c",
+			"--compress=1",
+			"--file=$backupFile"
+		)
+
+		&pg_dump $pgArgs
+
+		if ($LASTEXITCODE -ne 0) { throw "pg_dump exited with code $LASTEXITCODE." }
+		Write-Host "Backed up $backupFile"
+
+		$pgArgs = @(
+			"--list",
+			$backupFile
+		)
+
+		&pg_restore $pgArgs
+
+		if ($LASTEXITCODE -ne 0) { throw "pg_restore exited with code $LASTEXITCODE." }
+		Write-Host "Validated $backupFile"
+
+		$doneFile = $backupFile + ".done"
+		$(Get-Date -format "yyyy-MM-dd HH:mm:ss") | Set-Content $doneFile
+		Write-Host "Wrote $doneFile"
+	}
+
+	Write-Host -ForegroundColor Green "Backed up successfully into $backupFile"
 }
 catch {
-    throw
+	throw
 }
 finally {
 	Write-Host "Took: " ((Get-Date) - $start)
